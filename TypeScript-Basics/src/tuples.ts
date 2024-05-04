9ed2272a81eeedbcc1cf3e886ea7d70846d58d7b
Nicolas Mayorga <namayorgav@gmail.com>
2024-05-04 12:53:04 -0500

    [ADD] Tuples topic

---
+++ TypeScript-Basics/src/tuples.ts
@@ -0,0 +1,11 @@
+// -> Fixed length array and fixed type
+
+let persony: [number, string] = [1, "John"]
+
+function getPerson(): [number, string] {
+  return [1, "John"]
+}
+
+let date: readonly [number, number, number] = [2020, 12, 31]
+
+let susany: [string, number?] = ["Susan"]
