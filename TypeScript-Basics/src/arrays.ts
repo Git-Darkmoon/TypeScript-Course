afd5b0a752162e555da012363ce32a09f689362b
Nicolas Mayorga <namayorgav@gmail.com>
2024-05-17 18:22:11 -0500

    [REFACTOR] Updated file locations after completing basics tutorial.

--- TypeScript-Basics/src/arrays.ts
+++
@@ -1,7 +0,0 @@
-let prices: number[] = [100, 23, 232]
-// prices.push('a')
-
-let products: string[] = ["a", "b", "c"]
-
-let names = ["peter", "john"]
-let array: (string | boolean)[] = ["a", true]
