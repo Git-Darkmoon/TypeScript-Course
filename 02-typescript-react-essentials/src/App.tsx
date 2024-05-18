b45ec190329f74ff7988fdf9c4c236312170c432
Nicolas Mayorga <namayorgav@gmail.com>
2024-05-17 23:47:20 -0500

    [UPDATE] Import path.

--- 02-typescript-react-essentials/src/App.tsx
+++ 02-typescript-react-essentials/src/App.tsx
@@ -1,9 +1,11 @@
-import Component from "./starter/01-return"
+import Component from "./starter/02-props"
 
 function App() {
   return (
     <main>
-      <Component />
+      <Component title="peter" id={123}>
+        <h2>Hello world</h2>
+      </Component>
     </main>
   )
 }
