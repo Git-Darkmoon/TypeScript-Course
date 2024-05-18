1976d5b562ac2db4f22fd365d72dd59321cfdb8a
Nicolas Mayorga <namayorgav@gmail.com>
2024-05-17 23:46:28 -0500

    [ADD] Learned component returns.

--- 02-typescript-react-essentials/src/starter/01-return/index.tsx
+++ 02-typescript-react-essentials/src/starter/01-return/index.tsx
@@ -1,9 +1,12 @@
-function Component() {
+// Usually the return type is inferred by TS
+// but we can specify it explicitly
+
+function Component(): JSX.Element | null | string {
   return (
     <div>
       <h2>React & Typescript</h2>
       <h2>Return Type</h2>
     </div>
-  );
+  )
 }
-export default Component;
+export default Component
