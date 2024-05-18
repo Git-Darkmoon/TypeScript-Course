e08b6371d7d9ad0b151a03b25371b92efb791f3a
Nicolas Mayorga <namayorgav@gmail.com>
2024-05-17 23:47:06 -0500

    [ADD] Understood component props with TS and using children special prop.

--- 02-typescript-react-essentials/src/starter/02-props/index.tsx
+++ 02-typescript-react-essentials/src/starter/02-props/index.tsx
@@ -1,9 +1,24 @@
-function Component() {
+// import { ReactNode } from "react"
+
+import { type PropsWithChildren } from "react"
+
+type ComponentProps = {
+  title: string
+  id: number
+  // children?: React.ReactNode -> for no importing
+  // children?: ReactNode
+}
+
+// an alternative of setting ourselves the children, would be use the special type PropsWithChildren
+type iProps = PropsWithChildren<ComponentProps>
+
+function Component({ title, id, children }: iProps) {
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>Props</h2>
+      <h1>Title: {title}</h1>
+      <h2>ID: {id}</h2>
+      {children}
     </div>
-  );
+  )
 }
-export default Component;
+export default Component
